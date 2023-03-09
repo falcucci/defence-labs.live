dd478ead53264ab6fb7dfe711b9e3bb3e402ba6d
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-03-09 15:08:05 +0100

    add the embed spotify playlist
    changes:
    
    - bun.lockb
    - package.json
    - pages/_app.js
    - pages/index.js
    - styles/globals.css
    - yarn.lock

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,22 @@
+import React from "react";
+import { SpotifyEmbed } from "spotify-embed";
+import Head from "next/head";
+
 export default function Home() {
-  return <div>Hello World</div>
+  return (
+    <div style={{ margin: "0", padding: "0", background: "#000" }}>
+      <Head>
+        <title>Country Flags</title>
+        <meta name="description" content="Get country flags" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <SpotifyEmbed
+        src="https://open.spotify.com/playlist/7t7AYUmCzKFEFDmP3IDn9a?si=1d390f3a33bc48e9"
+        theme="dark"
+        size="custom"
+        width={"100%"}
+        height={"900"}
+      />
+    </div>
+  );
 }
