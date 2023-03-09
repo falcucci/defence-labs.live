9cfe95fc7802992c0a85131065df62303097628a
Alexsander Falcucci <alexsander.santos@luizalabs.com>
2023-03-09 17:53:31 +0100

    update home page title and description
    updates the title and description of the home page. it also removes the `si` parameter from the spotify embed url.
    
    - change home page title from `country flags` to `defence labs live`
    - change home page description from `get country flags` to `defence labs live`
    - remove `si` parameter from spotify embed url `https://open.spotify.com/playlist/7t7ayumczkfefdmp3idn9a?si=1d390f3a33bc48e9` to `https://open.spotify.com/playlist/7t7ayumczkfefdmp3idn9a`

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Country Flags</title>
-        <meta name="description" content="Get country flags" />
+        <title>Defence Labs Live</title>
+        <meta name="description" content="defence labs live" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <SpotifyEmbed
-        src="https://open.spotify.com/playlist/7t7AYUmCzKFEFDmP3IDn9a?si=1d390f3a33bc48e9"
+        src="https://open.spotify.com/playlist/7t7AYUmCzKFEFDmP3IDn9a"
         theme="dark"
         size="custom"
         width={"100%"}
